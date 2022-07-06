ddd7fae6d9903053407a8e486506d4aa1028204a
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-06 13:10:05 -0300

    cool

---
+++ PositionIdle.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="app">
+   <h2>x = {{xValue}}</h2>
+</div>
+</template>
+
+<script>
+export defalut {
+    name:"app"
+    data() {
+        return {
+            xValue: 0;
+        }
+    }
+}
+<script>
